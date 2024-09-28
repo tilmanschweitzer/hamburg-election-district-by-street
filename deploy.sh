c3e04ef385b206ece0db61c0eb95f46cb485a509
Tilman Schweitzer <tilman.schweitzer@qaware.de>
2024-09-28 17:32:02 +0200

    deploy.sh: Replace title and add additional URL

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i '' 's/Vite App/Hamburg Wahlbezirke 2025/g' dist/index.html
+
 gcloud storage cp dist/*.*  gs://hamburg-election-district-by-street-search/
 
 gcloud storage rm gs://hamburg-election-district-by-street-search/assets/*
@@ -7,4 +9,5 @@ gcloud storage rm gs://hamburg-election-district-by-street-search/assets/*
 gcloud storage cp --gzip-local-all dist/assets/*.*  gs://hamburg-election-district-by-street-search/assets
 
 commit_hash=$(git show HEAD~2 --pretty=format:"%h" --no-patch)
-echo "Open https://storage.googleapis.com/hamburg-election-district-by-street-search/index.html?hash=${commit_hash}"
+echo "Open https://storage.googleapis.com/hamburg-election-district-by-street-search/?hash=${commit_hash}"
+echo "Open https://hamburg-election-district-by-street.tilmanschweitzer.de/?hash=${commit_hash}"
