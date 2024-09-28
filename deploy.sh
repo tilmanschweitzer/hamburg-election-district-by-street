fd225bbb5d2b5c1348fa5d2f3f04a71d7dec4ec2
Tilman Schweitzer <tilman.schweitzer@qaware.de>
2024-09-28 14:31:09 +0200

    Print url with git hash after deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#npm run build
-
 gcloud storage cp dist/*.*  gs://hamburg-election-district-by-street-search/
 
 gcloud storage rm gs://hamburg-election-district-by-street-search/assets/*
 
 gcloud storage cp --gzip-local-all dist/assets/*.*  gs://hamburg-election-district-by-street-search/assets
+
+commit_hash=$(git show HEAD~2 --pretty=format:"%h" --no-patch)
+echo "Open https://storage.googleapis.com/hamburg-election-district-by-street-search/index.html?hash=${commit_hash}"
