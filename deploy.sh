3ba8594f39285a7932a927233ab1127e845eefcd
Tilman Schweitzer <tilman.schweitzer@qaware.de>
2024-10-01 16:24:50 +0200

    Improve deploy.sh with new bucket

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+
+# Old setup
+# bucket_name="hamburg-election-district-by-street-search"
+# bucket_domain="hamburg-election-district-by-street.tilmanschweitzer.de"
+
+bucket_name="hamburg-election-district-by-street.tilmanschweitzer.de"
+bucket_domain="${bucket_name}"
+
 sed -i '' 's/Vite App/Hamburg Wahlbezirke 2025/g' dist/index.html
 
-gcloud storage cp dist/*.*  gs://hamburg-election-district-by-street-search/
+gcloud storage cp dist/*.*  "gs://${bucket_name}/"
 
-gcloud storage rm gs://hamburg-election-district-by-street-search/assets/*
+gcloud storage rm "gs://${bucket_name}/assets/*"
 
-gcloud storage cp --gzip-local-all dist/assets/*.*  gs://hamburg-election-district-by-street-search/assets
+gcloud storage cp --gzip-local-all dist/assets/*.*  "gs://${bucket_name}/assets"
 
 commit_hash=$(git show HEAD~2 --pretty=format:"%h" --no-patch)
-echo "Open https://storage.googleapis.com/hamburg-election-district-by-street-search/?hash=${commit_hash}"
-echo "Open https://hamburg-election-district-by-street.tilmanschweitzer.de/?hash=${commit_hash}"
+echo "Open https://storage.googleapis.com/${bucket_name}/index.html?hash=${commit_hash}"
+echo "Open https://${bucket_domain}/?hash=${commit_hash}"
